eb66f10e04eb275dba5dea3c8ad450fb5425e208
yonger <1046788379@qq.com>
2017-06-23 12:15:28 +0800

    获取localStorage的的数组,相同项目的id,以及id对应的数量.并通过数组的join方法转换成对应的格式

--- src/common/goodscarhelper.js
+++ src/common/goodscarhelper.js
@@ -3,16 +3,10 @@ const key = 'goodslist'
 //保存我们的商品信息到我们的localStorage当中
 export function addGoods (goodsObj){
     //第一个次获取可能为null
-    //console.log(goodsObj)
-    //console.log(localStorage.getItem(key))
     const goodsArr = JSON.parse(localStorage.getItem(key)||"[]")
-    //console.log(localStorage.getItem(key))
     goodsArr.push(goodsObj)
-    //console.log(goodsArr)
-    //console.log(JSON.stringify(goodsArr))
     localStorage.setItem(key,JSON.stringify(goodsArr))
 }
-
 //获取我们的localStorage 当中的count 数量,
 //这是带返回值的函数
 export function getCounts(){
@@ -23,4 +17,8 @@ export function getCounts(){
     })
     //返回总共的商品数量,为我们的跟组件所用
     return totalCount
+}
+//得到localStorage里面的数组
+export function getLocalStorageArr(){
+    return JSON.parse(localStorage.getItem(key)||"[]")
 }
--- src/components/shopcar/shopcar.vue
+++ src/components/shopcar/shopcar.vue
@@ -8,4 +8,44 @@
 </style>
 
 <script>
+    import common from '../../common/common.js'
+    //导入本地商品存储数组
+    import {getLocalStorageArr} from '../../common/goodscarhelper.js'
+
+    export default {
+        data(){
+            return {
+
+            }
+        },
+        created(){
+        this.getGoodsInfo()
+        },
+        methods:{
+            getGoodsInfo(){
+//                1.获取localStorage中的数组
+//                [{"goodsId":"90","count":4},{"goodsId":"90","count…dsId":"95","count":1},{"goodsId":"95","count":5}]
+                const goodsArray = getLocalStorageArr()
+//                2.合并数组中的数量,整成一个新的数据{87:3,90:5,88:3}
+                var goodsObj = {}
+                for(var i = 0;i<goodsArray.length;i++){
+                    const goodsId = goodsArray[i]['goodsId']
+                    if(goodsObj[goodsId]){
+                        var oldValue = goodsObj[goodsId]
+                        var newValue = oldValue+ parseInt(goodsArray[i]['count'])
+                        goodsObj[goodsId] = newValue
+                    }else {
+                        goodsObj[goodsId] = parseInt(goodsArray[i]['count'])
+                    }
+                }
+//                3.在新的数组中,获取么个商品的id组成88,89,90 因为后台需要
+                var idsArr = []
+                for(var key in goodsObj){
+                    idsArr.push(key)
+                }
+                var idsString= idsArr.join(',')
+//                4.发送ajax请求,获取数据
+            }
+        }
+    }
 </script>
